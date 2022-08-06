a22e00d30434cda929b3af98fd9418dee3d37d87
Sergey <sergey@siemonster.com>
2022-08-06 11:40:07 +0300

    fixed websockets

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import express, { Router } from 'express';
 import RequestId from 'express-request-id';
 import { createProxyMiddleware  } from 'http-proxy-middleware';
 
-import { register, Counter, Gauge, collectDefaultMetrics } from 'prom-client';
+import { register, Gauge, collectDefaultMetrics } from 'prom-client';
 
 
 const GATEWAY_PORT = 4000;
@@ -14,12 +14,6 @@ const TBOT_DIR = '/home/sergey/apps/tbot-app';
 const GRAFANA_KEY = 'eyJrIjoieWFPZnFGMVdiMDVEek5HNXlIUTlIZVI0WFA5VUNnUUIiLCJuIjoiYWRtaW4iLCJpZCI6MX0=';
 
 
-// const exceptionsCounterMetric = new Counter({
-//     name: 'gateway_active_connections',
-//     help: 'Example of a counter',
-//     labelNames: ['code'],
-// });
-
 const requestsActiveMetric = new Gauge({
     name: 'gateway_connections_active',
     help: 'Example of a gauge',
@@ -29,54 +23,20 @@ const requestsActiveMetric = new Gauge({
 
 async function start() {
     const app = express();
+    const server = http.createServer(app);
+
     app.use(RequestId());
     app.use((req, res, next) => {
         console.log(`Request: ${req.headers.host},  ${req.url}`);
         next();
     })
 
-    app.use('/tbot', express.static(TBOT_DIR));
+    setupBitwarden(app);
+    setupTelegram(app);
+    setupMetrics(app, server);
 
-    app.use('/tbot-api/**', (req, res) => { res.sendStatus(501); });
-
-    app.use('/bitwarden/**', createProxyMiddleware({ 
-        target: 'http://localhost:4002',
-        changeOrigin: false,
-    }));
-
-    app.use('/bitwarden', (req, res) => { res.redirect('/bitwarden/'); });
-
-    app.use('/grafana', createProxyMiddleware({ 
-        target: 'http://localhost:3000',
-        changeOrigin: false,
-        onProxyReq: (proxyReq, req, res) => {
-            // proxyReq.setHeader('authorization', `Bearer ${GRAFANA_KEY}`);
-        }
-    }));
-
-    app.use('/prometheus', createProxyMiddleware({ 
-        target: 'http://localhost:9090',
-        changeOrigin: true,
-        pathRewrite: {'^/prometheus' : ''},
-    }));
-
-    app.get('/metrics', async (req, res) => {
-        try {
-            res.set('Content-Type', register.contentType);
-            res.end(await register.metrics());
-        } catch (ex) {
-            res.status(500).end(ex);
-        }
-    });
-
-    const server = http.createServer(app).listen(GATEWAY_PORT);
+    server.listen(GATEWAY_PORT);
     console.log(`Gateway server listening to ${GATEWAY_PORT}`);
-
-    setInterval(() => {
-        server.getConnections(function(err, count) {
-            requestsActiveMetric.set(count);
-         });
-    }, 1000);
 
     // app.listen(GATEWAY_PORT, () => 
     //     console.log(`Gateway server listening to ${GATEWAY_PORT}`));
@@ -101,25 +61,66 @@ async function start() {
     // }).listen(GATEWAY_PORT);
 }
 
-async function startMetrics() {
-    // collectDefaultMetrics({
-    //     gcDurationBuckets: [0.001, 0.01, 0.1, 1, 2, 5], // These are the default buckets.
-    // });
-    
-    // Setup server to Prometheus scrapes:
-    const app = express();
 
-    // app.get('/metrics/counter', async (req, res) => {
-    //     try {
-    //         res.set('Content-Type', register.contentType);
-    //         res.end(await register.getSingleMetricAsString('test_counter'));
-    //     } catch (ex) {
-    //         res.status(500).end(ex);
-    //     }
-    // });
-    
-    app.listen(METRICS_PORT, () => 
-        console.log(`Metrics server listening to ${METRICS_PORT}, metrics exposed on /metrics endpoint`));
+function setupTelegram(app) {
+    app.use('/tbot', express.static(TBOT_DIR));
+
+    app.use('/tbot-api/**', (req, res) => { res.sendStatus(501); });
+}
+
+
+function setupBitwarden(app) {
+    app.use(createProxyMiddleware('/bitwarden/notifications/hub/**', { 
+        target: 'http://localhost:4002',
+        changeOrigin: false,
+    }));
+
+    app.use(createProxyMiddleware('/bitwarden/notifications/hub', { 
+        target: 'ws://localhost:3012',
+        changeOrigin: false,
+        ws: true,
+    }));
+
+    app.use('/bitwarden/**', createProxyMiddleware({ 
+        target: 'http://localhost:4002',
+        changeOrigin: false,
+    }));
+
+    app.use('/bitwarden', (req, res) => { res.redirect('/bitwarden/'); });
+}
+
+
+function setupMetrics(app, server) {
+    app.use(createProxyMiddleware('/grafana', { 
+        target: 'http://localhost:3000',
+        changeOrigin: false,
+        ws: true,
+        logLevel : 'debug',
+        onProxyReq: (proxyReq, req, res) => {
+            // proxyReq.setHeader('authorization', `Bearer ${GRAFANA_KEY}`);
+        }
+    }));
+
+    app.use(createProxyMiddleware('/prometheus', { 
+        target: 'http://localhost:9090',
+        changeOrigin: false,
+        pathRewrite: {'^/prometheus' : ''},
+    }));
+
+    app.get('/metrics', async (req, res) => {
+        try {
+            res.set('Content-Type', register.contentType);
+            res.end(await register.metrics());
+        } catch (ex) {
+            res.status(500).end(ex);
+        }
+    });
+
+    setInterval(() => {
+        server.getConnections(function(err, count) {
+            requestsActiveMetric.set(count ?? 0);
+         });
+    }, 1000);
 }
 
 
